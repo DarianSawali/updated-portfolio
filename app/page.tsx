fa35c72ed82b2e23d5005079ea1bea20042c31fd
Darian Sawali <darianaxel@gmail.com>
2025-01-28 23:11:59 -0800

    home page, navbar, and about me

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import Hero from "@/components/main/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <main className="h-full w-full">
-        <div className="flex flex-col gap-20">
-          
+        <div className="flex flex-col max-w-[1400px] mx-auto gap-20">
+          <Hero/>
         </div>
       </main>
     </div>
