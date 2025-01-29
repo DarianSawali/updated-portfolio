5585cc52c71d4da2af91418c67f6ba0e03fe1b1e
Darian Sawali <darianaxel@gmail.com>
2025-01-28 21:55:19 -0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="h-full w-full">
+        <div className="flex flex-col gap-20">
+          
+        </div>
+      </main>
+    </div>
+  );
+}
